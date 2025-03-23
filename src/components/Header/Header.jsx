c016327917c6c87ec4e3e51db903d456b3d3918f
Simon Gebord <sgebord39@gmail.com>
2025-03-22 23:21:47 -0400

    Header structure added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import avatar from "../../assets/avatar.svg";
+import logo from "../../assets/logo.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="" className="header__logo" />
+      <p className="header__date-and-location">Date, Location</p>
+      <button className="header__add-clothes-btn">Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Simon Gebord</p>
+        <img src={avatar} alt="Simon Gebord" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
