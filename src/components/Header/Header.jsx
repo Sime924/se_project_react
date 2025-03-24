f8d7c210a17d766d285b1be56a21f835877f43db
Simon Gebord <sgebord39@gmail.com>
2025-03-24 15:31:55 -0400

    Modal buttons functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import avatar from "../../assets/avatar.svg";
 import logo from "../../assets/logo.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img src={logo} alt="" className="header__logo" />
       <p className="header__date-and-location">Date, Location</p>
-      <button className="header__add-clothes-btn">Add clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Simon Gebord</p>
         <img src={avatar} alt="Simon Gebord" className="header__avatar" />
