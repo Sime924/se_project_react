e2d07c1e919c6075b60e7ef78246e8b33f317c1b
Simon Gebord <sgebord39@gmail.com>
2025-07-01 12:50:43 -0400

    loginModal functionality complete

---
+++ src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+
+const handleEmailChange = (e) => {
+  setEmail(e.target.value);
+};
+
+const handlePasswordChange = (e) => {
+  setPassword(e.target.value);
+};
+
+return (
+  <ModalWithForm
+    title="Log in"
+    buttonText="Log in"
+    isOpen={isOpen}
+    onClose={onClose}
+    onSubmit={handleSubmit}
+  >
+    <label htmlFor="email" className="modal__label">
+      Email{" "}
+      <input
+        type="email"
+        className="modal__input"
+        id="email"
+        placeholder="email"
+        required
+        minLength="1"
+        maxLength="30"
+        onChange={handleEmailChange}
+        value={email}
+      />
+    </label>
+
+    <label htmlFor="password" className="modal__label">
+      Password{" "}
+      <input
+        type="password"
+        className="modal__input"
+        id="password"
+        placeholder="password"
+        required
+        onChange={handlePasswordChange}
+        value={password}
+      />
+    </label>
+  </ModalWithForm>
+);
