7e21aafef27a8e8c7d971c73a3aa6bcf88bedb85
Simon Gebord <sgebord39@gmail.com>
2025-08-28 22:44:11 -0400

    fixed redirect issue with the username link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -31,19 +31,20 @@ function Header({
         <ToggleSwitch />
         {isLoggedIn ? (
           <div className="header__user-container">
-            <Link to="/profile" className="header__link"></Link>
             <button
               className="header__add-clothes-btn"
               onClick={handleAddClick}
             >
               + Add clothes
             </button>
-            <p className="header__username">{currentUser?.name}</p>
-            <img
-              src={currentUser?.avatar}
-              alt={currentUser?.name}
-              className="header__avatar"
-            />
+            <Link to="/profile" className="header__link">
+              <p className="header__username">{currentUser?.name}</p>
+              <img
+                src={currentUser?.avatar}
+                alt={currentUser?.name}
+                className="header__avatar"
+              />
+            </Link>
           </div>
         ) : (
           <div className="header__auth-container">
